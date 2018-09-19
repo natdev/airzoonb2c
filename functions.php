64e86c6d6cd091fff9781be14efa142cd589ed8a
natdev <dordonne.nathanael@gmail.com>
2018-09-19 13:18:00 -0400

    deploiement

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 define("VERSION","1.0");
 function airzoon_files(){
+  wp_enqueue_script('bootstrap_js','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',NULL, VERSION, true);
   wp_enqueue_style('normalize',get_theme_file_uri('/css/nomalize.css'),NULL,VERSION,true);
+  wp_enqueue_style('bootstrap','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',array('nomalize'),VERSION);
   wp_enqueue_style('header_airzoon',get_theme_file_uri('/css/header.css'),array('nomalize','bootstrap'),VERSION);
   wp_enqueue_style('content_airzoon',get_theme_file_uri('/css/content.css'),array('nomalize', 'bootstrap', 'header_airzoon'),VERSION);
   wp_enqueue_style('footer_airzoon',get_theme_file_uri('/css/footer.css'),array('nomalize', 'bootstrap', 'header_airzoon','content_airzoon'),VERSION);
---
+++ templates-part/header/header.php
@@ -0,0 +1,16 @@
+<header>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-12">Logo</div>
+          <div class="col-lg-4 col-md-4 col-sm-12">Social media</div>
+        </div>
+        <div class="row">
+          <div class="col-lg-5 col-md-5 col-sm-12">Logo</div>
+          <div class="col-lg-8 col-md-8 col-sm-12">Logo</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
