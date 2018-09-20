184389ac8708bca7d3b756ce398b135f1f78854b
natdev <dordonne.nathanael@gmail.com>
2018-09-20 17:00:44 -0400

    création du header

--- front-page.php
+++ front-page.php
@@ -1,3 +1,82 @@
-<?php get_header(); ?>
-hello world !!
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+  <title>Document</title>
+</head>
+<body>
+    <header id="offer">
+       <div class="container">
+         <nav>
+           <ul>
+             <span>
+               <a href=""><img src="<?php echo get_template_directory_uri()."/images/logo-airzoon.png" ?>" alt=""></a>
+             </span>
+             <li>
+               <a href="">f</a>
+             </li>
+             <li>
+               <a href="">t</a>
+             </li>
+             <li>
+               <a href="">i</a>
+             </li>
+             <li>
+               <a href="">l</a>
+             </li>
+           </ul>
+         </nav>
+         <div id="promoM" class="row">
+           <div class="perfect-desc col-lg-5">
+             <h1>Perfect Spot.</h1>
+             <p class="font-weight-bold">
+               Bénéficier d'un code premium et surfez sans intéruption
+               et 4x plus vite.
+             </p>
+             <p>
+               Surfez en toute sécurité dans les portails airZoon chez l'ensemble de nos partenaires
+               en Guadeloupe, en Guyane et en Martinique.
+             </p>
+             <div class="btn btn-success">
+               ESSAI GRATUIT
+             </div>
+           </div>
+           <div id="offre" class="col-lg-7">
+            <div class="container">
+              <div class="row">
+                <div class="card-offre enable-offre col-lg-6 text-center">
+                  <h4 class="title">
+                    <span class="d-block">airZoon No Limit</span>
+                    ONE SHOT
+                  </h4>
+                  <p class="price">9,99€</p>
+                  <p class="description">
+                    Coupon premium d'une durée de 3 semaine de surf illimité et sans intéruption
+                  </p>
+                  <div class="btn btn-danger">
+                    Souscrire
+                  </div>
+                </div>
+                <div class="card-offre desable-offre col-lg-6 text-center">
+                  <h4 class="title">
+                    <span class="d-block">airZoon No Limit</span>
+                    Air
+                  </h4>
+                  <p class="price">9,99€</p>
+                  <p class="description">
+                    Abonnement mensuel pour surfer sans contrainte
+                  </p>
+                  <div class="btn btn-danger">
+                    Souscrire
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+         </div>
+       </div>
+    </header>
+    <?php wp_footer(); ?>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -1,13 +1,32 @@
 <?php
 define("VERSION","1.0");
 function airzoon_files(){
-  wp_enqueue_script('bootstrap_js','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',NULL, VERSION, true);
-  wp_enqueue_style('normalize',get_theme_file_uri('/css/nomalize.css'),NULL,VERSION,true);
-  wp_enqueue_style('bootstrap','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',array('nomalize'),VERSION);
-  wp_enqueue_style('header_airzoon',get_theme_file_uri('/css/header.css'),array('nomalize','bootstrap'),VERSION);
-  wp_enqueue_style('content_airzoon',get_theme_file_uri('/css/content.css'),array('nomalize', 'bootstrap', 'header_airzoon'),VERSION);
-  wp_enqueue_style('footer_airzoon',get_theme_file_uri('/css/footer.css'),array('nomalize', 'bootstrap', 'header_airzoon','content_airzoon'),VERSION);
-  wp_enqueue_style('mainstyle',get_theme_file_uri('style.css'), array('nomalize', 'bootstrap', 'header_airzoon','content_airzoon'),VERSION);
+  wp_enqueue_script('bootstrap_js',get_theme_file_uri('/js/bootstrap.bundle.min.js'),NULL, VERSION, true);
+
+  wp_enqueue_style('normalize',get_stylesheet_uri().'/css/normalize.css');
+  wp_enqueue_style('bootstrap_airzoon',get_template_directory_uri().'/css/bootstrap.min.css',array('normalize'), VERSION);
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i',NULL, VERSION);
+  wp_enqueue_style('header_airzoon',get_template_directory_uri().'/css/header.css',array('normalize','bootstrap_airzoon'), VERSION);
+  wp_enqueue_style('content_airzoon',get_template_directory_uri().'/css/content.css',array('normalize','bootstrap_airzoon','header_airzoon'),VERSION);
+  wp_enqueue_style('footer_airzoon',get_template_directory_uri().'/css/footer.css',array('normalize','bootstrap_airzoon','header_airzoon','content_airzoon'),VERSION);
+  wp_enqueue_style('mainstyle',get_stylesheet_uri(),array('normalize','bootstrap_airzoon','header_airzoon','content_airzoon','footer_airzoon'),VERSION);
 }
 
 add_action('wp_enqueue_scripts','airzoon_files');
+
+
+
+
+function airzoon_features(){
+    $defaults = array(
+        'height'      => 119,
+        'width'       => 119,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme','airzoon_features');
