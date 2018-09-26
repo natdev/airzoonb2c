2dcc37eb0c706e673ab063c29d9c848b392df887
natdev <dordonne.nathanael@gmail.com>
2018-09-26 16:27:14 -0400

    mise en place de la google map

--- front-page.php
+++ front-page.php
@@ -94,7 +94,7 @@
       <div class="row">
         <div class="col-lg-5">
           <div class="map">
-
+            <img src="<?php echo get_template_directory_uri()."/images/smartphone.png" ?>" alt="">
           </div>
         </div>
         <div class="col-lg-7">
